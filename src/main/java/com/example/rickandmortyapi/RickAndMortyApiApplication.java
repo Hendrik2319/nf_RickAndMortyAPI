c1b7e309ccb63d8f63e6a919d86b7c570f732304
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-09-19 11:00:19 +0200

    some indent modifications caused by IntelliJ and a vulnerability in Spring

--- src/main/java/com/example/rickandmortyapi/RickAndMortyApiApplication.java
+++ src/main/java/com/example/rickandmortyapi/RickAndMortyApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RickAndMortyApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RickAndMortyApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyApiApplication.class, args);
+    }
 
 }
