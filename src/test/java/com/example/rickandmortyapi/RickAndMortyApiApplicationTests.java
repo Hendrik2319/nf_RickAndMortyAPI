b75fefa96958ff41ef236d0fc74693f343c42835
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-09-19 10:59:33 +0200

    Initial commit

---
+++ src/test/java/com/example/rickandmortyapi/RickAndMortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rickandmortyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
