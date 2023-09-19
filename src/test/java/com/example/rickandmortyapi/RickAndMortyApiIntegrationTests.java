93587fa144d340696ab30be24ccd165839c8c239
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-09-19 15:56:16 +0200

    feat: added test for getAllCharacters

---
+++ src/test/java/com/example/rickandmortyapi/RickAndMortyApiIntegrationTests.java
@@ -0,0 +1,101 @@
+package com.example.rickandmortyapi;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.IOException;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RickAndMortyApiIntegrationTests {
+
+    private static MockWebServer mockWebServer;
+
+    @BeforeAll
+    static void setup() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @DynamicPropertySource
+    static void setUrlDynamically(DynamicPropertyRegistry reg) {
+        reg.add("com.example.rickandmortyapi.url", ()->mockWebServer.url("/").toString());
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getAllCharacters() throws Exception {
+        // Given
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setHeader("Content-Type", "application/json")
+                        .setBody("""
+                                {
+                                    "info": {
+                                        "count": 1,
+                                        "pages": 1,
+                                        "next": null,
+                                        "prev": null
+                                    },
+                                    "results": [
+                                        {
+                                            "id": 204,
+                                            "name": "Lisa",
+                                            "status": "Dead",
+                                            "species": "Alien",
+                                            "type": "",
+                                            "gender": "Female",
+                                            "origin": {
+                                                "name": "unknown",
+                                                "url": ""
+                                            },
+                                            "location": {
+                                                "name": "Immortality Field Resort",
+                                                "url": "https://rickandmortyapi.com/api/location/7"
+                                            },
+                                            "image": "https://rickandmortyapi.com/api/character/avatar/204.jpeg",
+                                            "episode": [
+                                                "https://rickandmortyapi.com/api/episode/26"
+                                            ],
+                                            "url": "https://rickandmortyapi.com/api/character/204",
+                                            "created": "2017-12-30T12:59:58.460Z"
+                                        }
+                                    ]
+                                }
+                                """)
+        );
+
+        // When
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .get("/api/characters")
+                )
+
+                // Then
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        [
+                            {
+                                "id": 204,
+                                "name": "Lisa",
+                                "species": "Alien"
+                            }
+                        ]
+                        """));
+    }
+
+}
